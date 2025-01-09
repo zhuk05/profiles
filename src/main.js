566550cd46dd3c9d0a345b17a0f5c2766bfa6de8
evzhukov <e.v.zhukov@viarune.com>
2025-01-09 14:02:26 +0700

    feat: vuex config

--- src/main.js
+++ src/main.js
@@ -1,15 +1,3 @@
-// import './assets/main.css'
-//
-// import { createApp } from 'vue'
-// import App from './App.vue'
-// import router from './router'
-//
-// const app = createApp(App)
-//
-// app.use(router)
-//
-// app.mount('#app')
-
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store/userStore.js'
